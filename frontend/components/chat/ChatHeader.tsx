a179308a23d689c1217622ac15d5053d35454fec
johnsonmercyi <50102624+johnsonmercyi@users.noreply.github.com>
2026-01-13 18:09:00 +0100

    Basic ChatHeader.tsx

---
+++ frontend/components/chat/ChatHeader.tsx
@@ -0,0 +1,7 @@
+export const ChatHeader = () => {
+  return (
+    <div className="border-b px-4 py-3 font-semibold">
+      <h1 className="text-xl font-bold">System Assistant</h1>
+    </div>
+  );
+};
